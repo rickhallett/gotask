397c4f6faa9c7fcfa35666885ca1737b801b7ac8
whitefire0 <rickhallett@live.co.uk>
2019-04-06 08:38:58 +0100

    simplify development strategy: basic crud app first

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ Users need to register
 • Users to be notified on creation or updates to tasks
 • Use JWT as authentication layer for the API
 • Describe a deployment process for this application
+
+Naive Thought Process:
+
+Create basic todo app first, before adding users, email confirmation, or authentication. Reasoning: learning Go, Echo, Quasar and Vue in one weekend is already a big task. Create a MVP first, extend later.
+
+Step 2:
+
+Register provides the route for registration and email confirmation
+Login provides the route for authentication
+Authentication is persisted by JWT
+
+GET     /user/todos         show all user todos, with todo ids stored within the HTML elements as names or data attributes
+POST    /user/todos/new     add user todo by multipart form, with :id dynamically determined by page javascript (Leave subtodos until v0.1 POC achieved - unnecessary complexity)
+GET     /user/todos/:id     get user todo by id (could add an associated notes textfield here?)
+PUT     /user/todos/:id     update todo by id
+DELETE  /user/todos/:id     delete todo by id
--- server.go
+++ server.go
@@ -2,49 +2,19 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 	"github.com/labstack/echo"
+	// "github.com/mattn/go-sqlite3"
 )
-
-// User to provide bind json payload into Go struct based on Content-Type JSON request header
-type User struct {
-	Fname string `json:"firstname"`
-	Lname string `json:"lastname"`
-	Username string `json:"username"`
-	Email string `json:"email"`
-}
-
-// DB can act as a mock DB until one is created
-type DB struct{
-	User []*User
-}
-
-// VDB Virtual Database
-var VDB DB
-
-func (db *DB) initDB(){
-	VDB.User = []*User{}
-}
-
-func (db *DB) insertUser(u *User) {
-	db.User = append(db.User, u)
-	for i, u := range db.User {
-		fmt.Printf("id: %v, user: %+v\n", i, u)
-	}
-}
-
-// Mock database global storage
-func init(){
-	VDB = DB{}
-	VDB.initDB()
-}
 
 func main() {
 	e := echo.New()
 	e.GET("/", indexHandler)
-	e.POST("/register", register)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/tasks", getTasks)
+    e.PUT("/tasks", putTask)
+    e.DELETE("/tasks/:id", deleteTask)
+
+	e.Logger.Fatal(e.Start(":8888"))
 
 }
 
@@ -52,15 +22,15 @@ func indexHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Welcome to GoTask!")
 }
 
-func register(c echo.Context) error {
-	u := new(User)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	
-	VDB.insertUser(u)
-	return c.JSON(http.StatusCreated, u)
+func getTasks(c echo.Context) error {
+	return c.JSON(200, "GET Tasks")
 }
 
+func putTask(c echo.Context) error {
+	return c.JSON(200, "PUT Tasks")
+}
 
+func deleteTask(c echo.Context) error {
+	return c.JSON(200, "DELETE Task "+c.Param("id"))
+}
 
