6d9a99048c91fc8a4b1cf6fc2da2f259ccbeb3d4
whitefire0 <rickhallett@live.co.uk>
2019-04-06 08:51:01 +0100

    integrate sqlite3 db with init and migrate functions

--- server.go
+++ server.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+
+	"gotask/handlers"
+
 	"github.com/labstack/echo"
-	// "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", indexHandler)
 
+	db := initDB("gotask.db")
+	migrate(db)
+
+	e.GET("/", indexHandler)
 	e.GET("/tasks", getTasks)
     e.PUT("/tasks", putTask)
     e.DELETE("/tasks/:id", deleteTask)
 
 	e.Logger.Fatal(e.Start(":8888"))
 
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+	CREATE TABLE IF NOT EXISTS tasks(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name VARCHAR NOT NULL
+	);
+	`
+
+	_, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func indexHandler(c echo.Context) error {
